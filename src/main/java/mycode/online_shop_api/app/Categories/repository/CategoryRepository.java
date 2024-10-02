6394780c552b966ae706d85ff48f055fe501b7eb
Badescu Oliver <olibadescu@gmail.com>
2024-10-02 20:39:58 +0300

    f

--- src/main/java/mycode/online_shop_api/app/Categories/repository/CategoryRepository.java
+++ src/main/java/mycode/online_shop_api/app/Categories/repository/CategoryRepository.java
@@ -4,4 +4,5 @@ import mycode.online_shop_api.app.categories.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
 }
